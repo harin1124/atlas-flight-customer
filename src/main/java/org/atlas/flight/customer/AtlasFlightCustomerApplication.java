f3df787352f16ca56e6bef7e36297d979af40957
harin1124 <lolrin1124@gmail.com>
2026-04-09 17:23:07 +0900

    init

---
+++ src/main/java/org/atlas/flight/customer/AtlasFlightCustomerApplication.java
@@ -0,0 +1,13 @@
+package org.atlas.flight.customer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AtlasFlightCustomerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AtlasFlightCustomerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/atlas/flight/customer/AtlasFlightCustomerApplicationTests.java
@@ -0,0 +1,13 @@
+package org.atlas.flight.customer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AtlasFlightCustomerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
