975aa136f63410dab5f7af28a4f1c8e1f6e4d8ca
harin1124 <lolrin1124@gmail.com>
2026-04-13 17:58:26 +0900

    feat: 고객 등록 기능 개발 중 임시 커밋
    - 고객 등록 요청 DTO 생성
    - 고객 등록 요청 API 엔드포인트 생성
    - 유효성 검사를 위한 FormatValidation 으로 정규식 및 메시지 정리

--- src/main/java/org/atlas/flight/customer/domain/customer/controller/CustomerController.java
+++ src/main/java/org/atlas/flight/customer/domain/customer/controller/CustomerController.java
@@ -2,14 +2,13 @@ package org.atlas.flight.customer.domain.customer.controller;
 
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
+import jakarta.validation.Valid;
 import org.atlas.flight.core.ApiResponse;
 import lombok.RequiredArgsConstructor;
+import org.atlas.flight.customer.domain.customer.dto.request.CustomerCreateRequest;
 import org.atlas.flight.customer.domain.customer.entity.Customer;
 import org.atlas.flight.customer.domain.customer.service.CustomerService;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/customers")
@@ -18,10 +17,16 @@ import org.springframework.web.bind.annotation.RestController;
 public class CustomerController {
 	private final CustomerService customerService;
 	
-	@GetMapping("/{id}")
+	@GetMapping("/{customerId}")
 	@Operation(summary = "고객 조회")
-	public ApiResponse<Customer> getCustomer(@PathVariable("id") String userId) {
-		System.out.println("성공");
-		return ApiResponse.success(customerService.getCustomer(userId));
+	public ApiResponse<Customer> getCustomer(@PathVariable String customerId) {
+		return ApiResponse.success(customerService.getCustomer(customerId));
+	}
+
+	@PostMapping
+	@Operation(summary = "고객 등록")
+	public ApiResponse<Void> createCustomer(@Valid @RequestBody CustomerCreateRequest request) {
+		System.out.println("request ::: " + request);
+		return ApiResponse.success();
 	}
 }
